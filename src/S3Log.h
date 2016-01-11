6ec50f4d96f7185755a97de1c6caaf280a9ba1b8
Adam Lee <ali@pivotal.io>
2016-01-11 11:09:54 +0800

    print segid into log

--- src/S3Log.h
+++ src/S3Log.h
@@ -25,8 +25,8 @@ LOGLEVEL getLogLevel(const char* v);
 
 #define PRINTFUNCTION(i, format, ...) LogMessage(i, format, __VA_ARGS__)
 
-#define LOG_FMT "[%s](%0X)%s:%d  "
-#define LOG_ARGS(LOGLEVELSTR) LOGLEVELSTR, pthread_self(), __FILE__, __LINE__
+#define LOG_FMT "[%s]#%d#(%0X)%s:%d  "
+#define LOG_ARGS(LOGLEVELSTR) LOGLEVELSTR, s3ext_segid, pthread_self(), __FILE__, __LINE__
 #define NEWLINE "\n"
 
 #define S3DEBUG(message, args...)    \
